cfbde05980354bf77f182a6b086321cfd7b4bc82
Marco Bejarano <marco.bejarano03@gmail.com>
2025-06-11 12:23:17 -0700

    created the geometric sequence logic

--- Script.py
+++ Script.py
@@ -14,3 +14,21 @@ def generate_arithmetic(first_term, common_diff, num_terms):
 
     sequence_sum = sum(sequence)
     return sequence, sequence_sum
+
+def generate_geometric(first_term, common_ratio, num_terms):
+    if num_terms <= 0:
+        return [], 0
+
+    if first_term == 0:
+        return [0] * num_terms, 0
+
+    sequence = []
+    for i in range(num_terms):
+        term = first_term * (common_ratio ** i)
+        sequence.append(term)
+
+    product = 1
+    for term in sequence:
+        product *= term
+
+    return sequence, product
