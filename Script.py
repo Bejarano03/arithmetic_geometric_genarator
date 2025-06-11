d3dea745f12395e8dfd303f9c461a4db947e89e6
Marco Bejarano <marco.bejarano03@gmail.com>
2025-06-11 12:18:47 -0700

    created arithmetic sequence function

--- Script.py
+++ Script.py
@@ -3,3 +3,14 @@ Marco Bejarano Oseguera
 06/11/2025
 """
 
+def generate_arithmetic(first_term, common_diff, num_terms):
+    if num_terms <= 0:
+        return [], 0
+
+    sequence = []
+    for i in range(num_terms):
+        term = first_term + (i * common_diff)
+        sequence.append(term)
+
+    sequence_sum = sum(sequence)
+    return sequence, sequence_sum
